aec77dfc9f1e9a8f3615d693a0b268457cc167eb
Rafael R Ferreira <rafaelrferreira@gmail.com>
2022-04-27 18:16:38 -0300

    Added examples ES6 operations and structures

--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
-console.log('oks')
+// Var, let and const
+var value = 8080;
+let justCode = "Let the code be made it";
+const code = "Code Day and Night!";
+
+// Array operations - for.each / map / reduce / filter / findIndex
+
+// Arrow Function - Anonimous Function
+
+// Parameter default value 
+
+// Template String
+
+// Object Short Syntax
+
+// Destructuring
+
+// Spread/Rest
+
+
+console.log('Hands On ES6+. Ready to go!')
